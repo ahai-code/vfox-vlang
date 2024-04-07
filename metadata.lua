0a1a476e3675ab9b330a42e85aa945b398577be2
ahai <1149956305@qq.com>
2024-04-07 20:38:41 +0800

    update

--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "vlang"
 --- Plugin version
-PLUGIN.version = "0.1.0"
+PLUGIN.version = "0.1.1"
 --- Plugin homepage
 PLUGIN.homepage = "https://github.com/ahai-code/vfox-vlang"
 --- Plugin license, please choose a correct license according to your needs.
