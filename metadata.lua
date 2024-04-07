95bb626e3bb9b0b909a845e4bfa45d7387dc5ea5
ahai <1149956305@qq.com>
2024-04-07 20:14:43 +0800

    initial plugin

--- metadata.lua
+++ metadata.lua
@@ -3,15 +3,15 @@ PLUGIN = {}
 
 --- !!! MUST BE SET !!!
 --- Plugin name
-PLUGIN.name = "your plugin name"
+PLUGIN.name = "vlang"
 --- Plugin version
-PLUGIN.version = "0.0.1"
+PLUGIN.version = "0.1.0"
 --- Plugin homepage
-PLUGIN.homepage = "https://github.com/version-fox/vfox-plugin-template"
+PLUGIN.homepage = "https://github.com/ahai-code/vfox-vlang"
 --- Plugin license, please choose a correct license according to your needs.
 PLUGIN.license = "Apache 2.0"
 --- Plugin description
-PLUGIN.description = "your plugin description"
+PLUGIN.description = "Vlang plugin"
 
 
 --- !!! OPTIONAL !!!
@@ -21,7 +21,7 @@ NOTE:
     If the plugin is not compatible with the current vfox version,
     vfox will not load the plugin and prompt the user to upgrade vfox.
  --]]
-PLUGIN.minRuntimeVersion = "0.3.0"
+PLUGIN.minRuntimeVersion = "0.3.2"
 --[[
 NOTE:
     If configured, vfox will check for updates to the plugin at this address,
@@ -33,8 +33,6 @@ NOTE:
     you can set this address to the manifest file address, so that the plugin can be updated automatically.
 
  --]]
-PLUGIN.manifestUrl = "https://github.com/version-fox/vfox-plugin-template/releases/download/manifest/manifest.json"
+PLUGIN.manifestUrl = "https://github.com/version-fox/ahai-code/vfox-vlang/releases/download/manifest/manifest.json"
 -- Some things that need user to be attention!
-PLUGIN.notes = {
-    "",
-}
+PLUGIN.notes = {}
